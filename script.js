030a859878bdeb68341b572b389ed95aef12f5b9
RichardChileya <richardchileyawk@gmail.com>
2022-09-17 04:03:32 +0200

    add revised javascript fixing errrors

--- script.js
+++ script.js
@@ -1,28 +1,10 @@
 /* Hamburger */
 const ham = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu-list');
-const logo = document.querySelector('.logo');
-const lists = document.querySelectorAll('.list');
 
 ham.addEventListener('click', () => {
   ham.classList.toggle('active');
-  menu.classList.toggle('active');
-  lists.classList.toggle('active');
-});
-
-logo.addEventListener('click', () => {
-  ham.classList.toggle('active');
-  menu.classList.toggle('in-active');
-  logo.classList.toggle('in-active');
-});
-
-const spreadList = [...lists];
-spreadList.forEach((list) => {
-  list.addEventListener('click', () => {
-    ham.classList.toggle('active');
-    menu.classList.toggle('active');
-    logo.classList.toggle('in-active');
-  });
+  menu.classList.toggle('action');
 });
 
 const speakers = [{
@@ -42,36 +24,37 @@ const speakers = [{
   speakerImage: './images/hivos.jpg',
   aboutSpeaker: 'Network Security specialist',
   about: 'Cyber Security specialist with experience in Oracle Cloud management',
-},
-];
-
-const speakers2 = [{
-
-  speakerName: 'Richard Kayford',
-  speakerImage: './images/speaker2.jpg',
-  aboutSpeaker: 'Cyber Security specialist',
-  about: 'Cyber Security specialist with experience in Oracle Cloud management',
+  class: 'hide',
 },
 {
-  speakerName: 'George Alex',
-  speakerImage: './images/potrait.jpg',
-  aboutSpeaker: 'Oracle Data Specialist',
-  about: 'Cyber Security specialist with experience in Oracle Cloud management',
-},
-{
-  speakerName: 'John Lingson',
-  speakerImage: './images/speaker2.jpg',
+  speakerName: 'Jae Flames',
+  speakerImage: './images/hivos.jpg',
   aboutSpeaker: 'Network Security specialist',
   about: 'Cyber Security specialist with experience in Oracle Cloud management',
+  class: 'hide',
+},
+{
+  speakerName: 'Jae Flames',
+  speakerImage: './images/hivos.jpg',
+  aboutSpeaker: 'Network Security specialist',
+  about: 'Cyber Security specialist with experience in Oracle Cloud management',
+  class: 'hide',
+},
+{
+  speakerName: 'Jae Flames',
+  speakerImage: './images/hivos.jpg',
+  aboutSpeaker: 'Network Security specialist',
+  about: 'Cyber Security specialist with experience in Oracle Cloud management',
+  class: 'hide',
 },
 ];
 
 const speakerIn = document.querySelector('.speaker1');
-speakers2.forEach((speaker) => {
+speakers.forEach((speaker) => {
   const div = document.createElement('div');
   div.innerHTML += `
   
-  <div class="speaker">
+  <div class="speaker ${speaker.class}">
     <img src="${speaker.speakerImage}" class= "potrait" >
     <div class="speaker-details">
       <h2>${speaker.speakerName}</h2>
@@ -84,20 +67,13 @@ speakers2.forEach((speaker) => {
   speakerIn.append(div);
 });
 
-const speakerIn2 = document.querySelector('.speaker2');
-speakers.forEach((speaker) => {
-  const div = document.createElement('div');
-  div.innerHTML += `
-  
-  <div class="speaker">
-    <img src="${speaker.speakerImage}" class= "potrait" >
-    <div class="speaker-details">
-      <h2>${speaker.speakerName}</h2>
-      <h3 class="aboutspeaker">${speaker.aboutSpeaker}</h3>
-      <p>${speaker.about}</p>
-    </div>
-  </div>
-  `;
-  div.classList.add('speaker');
-  speakerIn2.append(div);
+const more = document.querySelector('.more');
+const hide = document.querySelectorAll('.hide');
+
+more.addEventListener('click', () => {
+  more.classList.toggle('active');
+  const spread = [...hide];
+  spread.forEach((list) => {
+    list.classList.toggle('bactive');
+  });
 });
